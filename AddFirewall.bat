5ea0f633f97aadd5d79306cdfd2c4aba0e94daac
NCuongLinh <143623013+NCuongLinh@users.noreply.github.com>
2024-11-19 23:27:45 +0700

    Force run file as admin and rename for better clarity

---
+++ AddFirewall.bat
@@ -0,0 +1,66 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+setlocal enabledelayedexpansion
+
+:menu
+cls
+echo ==============================================
+echo Firewall Rule Adder
+echo ==============================================
+echo 1) Add firewall rules for all files in the current folder
+echo 2) Add firewall rules for all .exe files in the current folder
+echo 3) Exit
+echo ==============================================
+set /p choice="Choose an option (1-3): "
+
+if "%choice%"=="1" goto add_all_files
+if "%choice%"=="2" goto add_exe_files
+if "%choice%"=="3" exit
+echo Invalid choice! Please try again.
+pause
+goto menu
+
+:add_all_files
+echo Adding firewall rules for all files in the current folder...
+for %%F in (*) do (
+    echo Adding firewall rule for %%F
+    netsh advfirewall firewall add rule name="Allow %%~nF" dir=in action=allow program="%cd%\%%F" enable=yes
+)
+echo Done!
+pause
+goto menu
+
+:add_exe_files
+echo Adding firewall rules for all .exe files in the current folder...
+for %%F in (*.exe) do (
+    echo Adding firewall rule for %%F
+    netsh advfirewall firewall add rule name="Allow %%~nF" dir=in action=allow program="%cd%\%%F" enable=yes
+)
+echo Done!
+pause
+goto menu
